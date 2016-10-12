5ce7851d0e2792f295945d3fd9d45daaab0dfcf4
Miller Klitsner <mklitsner@gmail.com>
2016-10-11 23:46:24 -0700

    First commit of actual project

---
+++ Do the Loud/Assets/Manipulation/AudioTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTest : MonoBehaviour {
+
+	// Use this for initialization
+	GameObject MoveThisObjectWithSound;//public GameObject MoveThisObjectWithSound;
+	AudioSource audio;
+	public float background=0;
+	public float y=0;
+	void Start () {
+		MoveThisObjectWithSound = this.gameObject; //MoveThisObjectWithSound = GameObject.FindGameObjectWithTag("Cube");
+		audio = gameObject.AddComponent<AudioSource>();
+		audio.clip = Microphone.Start("Built-in Microphone", true, 1, 44100);
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+
+			if (audio.isPlaying) {
+			} else if (audio.clip.isReadyToPlay) {
+				audio.Play ();
+			} else {
+				audio.clip = Microphone.Start ("Built-in Microphone", true, 1, 44100);
+			}
+			y = audio.GetSpectrumData (128, 0, FFTWindow.BlackmanHarris) [64] * 1000000;
+		if (Input.GetKeyDown ("space")) {
+		background = audio.GetSpectrumData (128, 0, FFTWindow.BlackmanHarris) [64] * 1000000;
+		}else{
+
+		if (y >= 500+background) {
+				MoveThisObjectWithSound.transform.position += new Vector3 (0, 1, 0) * 5 * Time.deltaTime;
+
+			}
+
+		}
+	}
+	
+}
---
+++ Do the Loud/Assets/Manipulation/moveobject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveobject : MonoBehaviour {
+
+	public float soundLevel;
+	public bool thresholdBroken;
+	public float threshold=1000;
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		soundLevel = GetComponent<MicrophoneListener> ().soundLevel;
+
+		if (soundLevel> threshold && thresholdBroken==false){
+			thresholdBroken = true;
+			GetComponent<Rigidbody>().AddForce(Vector3.up *(soundLevel-threshold)*force, ForceMode.Impulse);
+
+
+		}
+
+		if (soundLevel < threshold) {
+			thresholdBroken = false;
+		}
+	}
+}
